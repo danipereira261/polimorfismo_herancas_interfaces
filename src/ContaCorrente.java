7a5680a0d6b388b5f129ce0df6ddf4984e767c59
danipereira261 <dpereira261@gmail.com>
2021-02-19 11:11:48 -0300

    aula 5 atualizada

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -9,4 +9,9 @@ public class ContaCorrente extends Conta {
         double valorASacar = valor + 0.20;
         return super.saca(valorASacar);
     }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
 }
